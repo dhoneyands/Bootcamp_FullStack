a26390fddc8680186cda2e7a5323c008fe9b03e3
dhoneyands <davidhoneyands@gmail.com>
2023-11-15 01:01:41 +0100

    Adding the server .js file for the delivery of the project

---
+++ server_entregable.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoAPIError.Client;
+const url = 'mongodb://mongodb-container:27017';
+
+//constants
+const hostname = '0.0.0.0';
+const port = 135;
+
+//requests routes
+app.get('/',function(req,res){
+    res.send('GET request to the Homepage');
+});
+app.post('/',function(req,res){
+    res.send('POST request to the Homepage');
+});
+app.get('/secret',function(req,res,next){
+    res.send('Print hello world');
+    console.log('This is a log message as a test')
+});
+
