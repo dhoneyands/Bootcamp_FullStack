622f1a8480f8139741810eb44db6315a5b3f2532
dhoneyands <davidhoneyands@gmail.com>
2023-11-15 23:06:42 +0100

    Incluyo codigo para la petición de todas las entradas

--- server_entregable.js
+++ server_entregable.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoAPIError.Client;
 const url = 'mongodb://mongodb-container:27017';
+const dbName=Food_Facts_Dataset;
 
 //constants
 const hostname = '0.0.0.0';
@@ -18,6 +19,25 @@ app.post('/',function(req,res){
 app.get('/secret',function(req,res,next){
     res.send('Print hello world');
     console.log('This is a log message as a test');
+});
+
+// Query para obtener todos los registros de la base de datos
+app.get('/api/get/all', async function(req,res){
+    try{
+        const dbo = client.db(dbName);
+        const query = {};
+        const result = await dbo.collection(collectionName).find(query).toArray();
+        if (result.length > 0){
+            res.status(200).send(result);
+            console.log("200 OK");
+        } else{
+            res.status(200).send("Collection is empty");
+        }
+        client.close();
+    } catch (err){
+        console.error(err);
+        res.status(500).send("An error occurred");
+    }
 });
 
 app.listen(port, hostname);
